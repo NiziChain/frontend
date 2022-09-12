565cded52595997f5962cd351441b3022be314ee
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-09-12 20:21:37 +0900

    add: 作品一覧

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import { Container } from '@mui/material'
-import React from 'react'
+
 
 const Footer = () => {
   return (
     <>
-      <Container className='flex justify-center items-center text-white bg-slate-400 h-9'>
+      <Container className='flex justify-center items-center text-white bg-slate-400 h-9 min-w-full'>
         &copy;Big Mam Pirates All Rights Reserved.
       </Container>
     </>
---
+++ src/components/WorksListDisplay.tsx
@@ -0,0 +1,77 @@
+import {
+  Container,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+
+// 作品リストの型
+export interface ContentList {
+  isOriginal: boolean
+  title: string
+  description: string
+  created_at: string
+}
+
+//
+interface Props {
+  title: string
+  array: ContentList[]
+}
+
+const WorksListDisplay = (props: Props) => {
+  const title = props.title
+  const array = props.array
+  return (
+    <>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Container
+            component='h2'
+            className='text-center font-semibold text-xm mt-4'
+          >
+            {title}
+          </Container>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Container>
+            {array.map((content) => {
+              return (
+                <Container
+                  component='ul'
+                  key={content['created_at']}
+                  className='flex max-w-md mt-4 mb-4 py-2 border-zinc-300 border rounded-xl'
+                >
+                  <Container className='max-w-xs mt-1'>
+                    <Container component='li'>
+                      作品名：{content['title']}
+                    </Container>
+                    <Container component='li'>
+                      情報：{content['description']}
+                    </Container>
+                    <Container component='li'>
+                      作成日：{content['created_at']}
+                    </Container>
+                  </Container>
+                  <Button
+                    variant='outlined'
+                    size='small'
+                    className='max-h-10 mt-4'
+                    onClick={() => {}}
+                  >
+                    詳細
+                  </Button>
+                </Container>
+              )
+            })}
+          </Container>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  )
+}
+
+export default WorksListDisplay
