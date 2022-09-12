04aa4f281f77196fe6106b7ea7cae0f9198e2d31
i-nakamura823 <i-nakamura@ist.osaka-u.ac.jp>
2022-09-12 18:49:13 +0900

    fix: footer位置の調整

--- src/pages/createId.tsx
+++ src/pages/createId.tsx
@@ -12,22 +12,24 @@ const CreateIdPage: NextPage = () => {
   return (
     <div>
       <Header />
-      <h1 className='font-bold mt-20 text-center'>IDの発行</h1>
-      <div className=' flex justify-center mt-20 text-center align-middle min-height'>
-        <button
-          onClick={()=>handleClick("親")}
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
-        >
-          親作品として登録
-        </button>
-        <button
-          onClick={()=>handleClick("子")}
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
-        >
-          子作品として登録
-        </button>
-      </div>
-      {/* Footerまだです */}
+      <div className='min-h-screen'>
+          <h1 className='font-bold mt-20 text-center'>IDの発行</h1>
+          <div className=' flex justify-center mt-20 text-center align-middle'>
+            <button
+              onClick={()=>handleClick("親")}
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+            >
+              親作品として登録
+            </button>
+            <button
+              onClick={()=>handleClick("子")}
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+            >
+              子作品として登録
+            </button>
+          </div>
+        </div>
+      <Footer />
     </div>
   )
 }
