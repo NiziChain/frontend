7d00fddf291cce41ea539b9bae45afe764ea6a34
i-nakamura823 <i-nakamura@ist.osaka-u.ac.jp>
2022-09-13 12:26:36 +0900

    feat: 親作品のロイヤリティ設定ページ

---
+++ src/pages/id/originalSetting.tsx
@@ -0,0 +1,76 @@
+import { NextPage } from 'next'
+import Header from '@/components/base/Header'
+import Footer from '@/components/base/Footer'
+import {
+  Stack,
+  TextField,
+  Grid,
+  Container,
+} from '@mui/material'
+import { useState, useCallback } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+
+const OriginalSetting: NextPage = () => {
+
+  const [royalty, setRoyalty] = useState(0)
+  const [hasRoyaltyError, setHasRoyaltyError] = useState(false)
+
+  const onSubmit = () => {
+    console.log('発行！')
+  }
+
+  const inputRoyalty = useCallback(
+    (event: { target: { value: any } }) => {
+      const inputValue = event.target.value
+      const isEmpty = inputValue === ''
+      setRoyalty(inputValue)
+      setHasRoyaltyError(isEmpty)
+    },
+    [setRoyalty, setHasRoyaltyError]
+  )
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // バリデーションをここに！
+    // const isEmptyName = royalty === 0
+
+    // if (isEmptyName) {
+    //   setHasRoyaltyError(true)
+    // }
+    
+    // Submit処理！！
+    console.log(`Sybmit Royalty: ${royalty}`)
+  }
+
+  return (
+    <>
+      <Header />
+      <Container className='min-h-screen text-center' component='div'>
+        <h1 className='mt-10 mb-4 text-center font-bold text-lg'>ID発行</h1>
+        <Grid container direction="column" alignItems="center">
+          <Stack component="form" noValidate onSubmit={handleSubmit} spacing={2} sx={{ m: 2, width: '25ch' }}>
+            <Grid  item className='mt-10 mb-4 text-center font-bold text-lg'>ロイヤリティの設定</Grid >
+            <TextField
+              type="text"
+              label="ロイヤルティ"
+              required
+              value={royalty}
+              error={hasRoyaltyError}
+              onChange={inputRoyalty}
+              helperText={hasRoyaltyError ? 'ロイヤリティを入力してください。' : ''}
+            />
+            <button
+              type="submit"
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+            >
+              ID発行
+            </button>
+          </Stack>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default OriginalSetting
