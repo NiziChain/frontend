71f605a1fdd05f1873470ff23f7a08783a008f45
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-09-14 13:09:01 +0900

    fix: tailwind反映

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import Header from '@/components/base/Header'
 import Footer from '@/components/base/Footer'
-
+import { Container } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <main className={styles.main}>
+      <Container className='min-h-screen'>
         <h1>HomePage</h1>
         <h2>Linkは上にあるので、そこから飛んでね!</h2>
-      </main>
+      </Container>
       <Footer />
     </>
   )
